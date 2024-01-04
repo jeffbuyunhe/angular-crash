eb20e7c315f0629e946b45493476fe34992d1f5a
Jeff He <jeffbuyunhe@hotmail.com>
2024-01-03 22:32:52 -0500

    added graph changing based on tick

--- src/app/game-body/game-body.component.ts
+++ src/app/game-body/game-body.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, inject, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
 import { isPlatformBrowser, NgIf } from "@angular/common";
 import { GameLogicService } from '../game-logic.service';
 import { CountUpModule } from 'ngx-countup';
 import { CountUpService } from '../count-up.service';
-import { NgChartsModule } from 'ng2-charts';
-import { ChartConfiguration, ChartOptions } from 'chart.js';
+import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-game-body',
@@ -17,21 +17,30 @@ import { ChartConfiguration, ChartOptions } from 'chart.js';
 /* Provides logic for game body, including the graph and multiplier number */
 export class GameBodyComponent {
   isBrowser: boolean = false;
+
   gameLogicService: GameLogicService = inject(GameLogicService);
   countUpService: CountUpService = inject(CountUpService);
 
   gameMultiplier: number;
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
   countUpOptions: object;
+  lineChartOptions: object;
   lineChartData: ChartConfiguration<'line'>['data'];
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     this.isBrowser = isPlatformBrowser(this.platformId);
-    this.gameMultiplier = this.gameLogicService.getGameMultiplier();
+
     this.countUpOptions = this.countUpService.getOptions();
+    this.lineChartOptions = this.gameLogicService.getOptions();
     this.lineChartData = this.gameLogicService.getChartData();
+
+    this.gameMultiplier = this.gameLogicService.getGameMultiplier();
   }
 
-  public lineChartOptions: ChartOptions<'line'> = {
-    responsive: false
-  };
+  incrementGameTick() {
+    this.gameLogicService.incrementGameTick();
+    this.chart?.update();
+  }
 }
--- src/app/game-logic.service.ts
+++ src/app/game-logic.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ChartConfiguration } from 'chart.js';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
 
 @Injectable({
   providedIn: 'root'
@@ -7,22 +7,45 @@ import { ChartConfiguration } from 'chart.js';
 /* Temporary service for game logic until backend portion is started */
 export class GameLogicService {
 
+  gameTick: number;
+
   gameMultiplier: number;
 
-  /* Test Labels */
-  labels: Array<any> = [
-    'Number'
-  ];
+  labels: Array<any> = [];
 
-  /* Test Datasets */
   datasets: Array<any> = [
     {
       data: [],
-      borderColor: 'black',
     }
   ]
 
-  constructor() { this.gameMultiplier = this.setGameMultiplier(); this.datasets[0].data = [this.getGameMultiplier()] }
+  options: ChartOptions<'line'> = {
+    elements: {
+      line: {
+        tension: 0.2
+      },
+      point: {
+        radius: 0
+      }
+    },
+    responsive: false,
+    plugins: {
+      legend: {
+        display: false
+      },
+    },
+  };
+
+  constructor() {
+    this.gameTick = 0;
+    this.gameMultiplier = this.setGameMultiplier();
+    this.updateChartData();
+  }
+
+  /* Returns bet multiplier for the game round */
+  getGameMultiplier(): number {
+    return this.gameMultiplier;
+  }
 
   /* Sets the bet multiplier for the game round */
   setGameMultiplier(): number {
@@ -33,13 +56,25 @@ export class GameLogicService {
     return <number><unknown>Math.min(1 / Math.random(), 5000).toFixed(2);
   }
 
-  /* Returns bet multiplier for the game round */
-  getGameMultiplier(): number {
-    return this.gameMultiplier;
+  /* Returns chart options */
+  getOptions(): object {
+    return this.options;
   }
 
   /* Returns chart data */
   getChartData(): ChartConfiguration<'line'>['data'] {
     return { labels: this.labels, datasets: this.datasets };
+  }
+
+  /* Increments game tick by one, updates chart */
+  incrementGameTick() {
+    this.gameTick++;
+    this.updateChartData();
+  }
+
+  /* Adds data to chart based off of game tick */
+  updateChartData() {
+    this.labels.push(this.gameTick);
+    this.datasets[0].data.push(1.0024 * Math.pow(1.0718, this.gameTick));
   }
 }
